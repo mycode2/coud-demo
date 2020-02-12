11f84a17907c654e616dd0953e5043116ef03e75
zhouchao <zhouchaoit@sina.com>
2020-02-12 21:56:44 +0800

    测试修改application命名空间的值是否生效

--- cust-service/src/main/java/com/zc/study/cust/controller/CustController.java
+++ cust-service/src/main/java/com/zc/study/cust/controller/CustController.java
@@ -1,6 +1,7 @@
 package com.zc.study.cust.controller;
 
 import com.zc.study.cust.common.CommonMDA;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/cust")
 public class CustController {
 
+    @Value("${basePackage}")
+    private String basePackage;
     @GetMapping("/getCustInfo")
     public String getCustInfo(){
         System.out.println(CommonMDA.CERTNUMFIVE_REGIONID);
         System.out.println(CommonMDA.GROUP_4G_SRC_APP_KEY);
         System.out.println(CommonMDA.GROUP_DST_SYS_ID);
         System.out.println(CommonMDA.PROD_SERVICE_OFFER_ID_COMP_PROD);
-        return "this is a test";
+        return "this is a test\t"+basePackage;
     }
 }
